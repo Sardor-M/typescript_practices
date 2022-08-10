4a1afcc1d42dac90a23721334d6db3fa0a3b81fa
Sardor Madaminov <65296404+Sardor-M@users.noreply.github.com>
2022-08-10 22:36:08 +0900

    source_code: added

--- GenericTypes.ts
+++ GenericTypes.ts
@@ -4,3 +4,48 @@
 
 
 */}
+
+___________________________________________________________________
+
+
+interface User {
+    name: string;
+    age: number,
+    occupation: string;
+}
+interface Admin {
+    name: string;
+    age: number;
+    occupation:string;
+}
+
+export type Person = User | Admin;
+
+export const persons: Person[] =[
+    {
+        name: "Steve Jobs",
+        age: 45,
+        occupation: "CEO of APPLE"
+    },
+     {
+        name: "Steve Nice",
+        age: 45,
+        occupation: "CEO of APPLE"
+    },
+     {
+        name: "Steve Bad",
+        age: 45,
+        occupation: "CEO of APPLE"
+    },
+     {
+        name: "Steve Serious",
+        age: 45,
+        occupation: "CEO of APPLE"
+    }
+];
+
+export function logPerson (user: Person) {
+    console.log (`- ${user.name}, ${user.age}, ${user.occupation}`);
+}
+
+persons.forEach(logPerson);
