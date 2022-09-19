7ed984a016e94be95e0e65e132bf9a0e02d3e0a2
Sardor Madaminov <sardor0968@gmail.com>
2022-09-19 21:58:17 +0900

    more expln {example}: added

--- function_return-type_learn.ts
+++ function_return-type_learn.ts
@@ -16,7 +16,9 @@ function sum (n1: number, n2: number) {
         This function has a void return type, Void function 
         in Typescript does not return anything ,but does its job. 
     And using void has the following benefits : 
-
+        - Improves the clarity of the code meaning that you dont have to read 
+          the whole function body to see if it returns anything. 
+        - Ensures type-safe: you will never assign the function with the void return type to a variable. 
 */}
 
 // 
@@ -26,3 +28,23 @@ function showResult(num: string) {
 }
 
 showResult(sum(5, 10));
+
+{/* 
+    Simple Example Case: 
+*/}
+
+function demoExample( a: number, b: number, c: number ) {
+    let addition = a + b + c;
+    return addition
+}
+
+console.log("Following are the demo results -> ");
+
+let returnVal1 = demoExample (100, 200, 300);
+console.log("Returned value of first one is :" + returnVal1);
+
+let returnVal2 = demoExample(200,100,600);
+console.log( "Returned value of second one is :"+ returnVal2);
+
+let returnVal3 = demoExample(300,400, 500);
+console.log("Returned value of third one is : "+ returnVal3);
