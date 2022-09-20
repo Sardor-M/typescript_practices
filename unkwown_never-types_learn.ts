9ba30e5b4f4961ffa92c797ae0f08d156ff38a83
Sardor Madaminov <sardor0968@gmail.com>
2022-09-20 22:14:13 +0900

    never type: added

--- unkwown_never-types_learn.ts
+++ unkwown_never-types_learn.ts
@@ -7,7 +7,6 @@
           type is more restrictive. 
 */}
 
-
 let loginInput: unknown;     
 let userName: string;
 
@@ -18,3 +17,28 @@ if (typeof loginInput === "string") {
     userName=loginInput;
 }
 
+// ---------------------------------------------------------------
+
+{/*
+    Never type explained: 
+      - Below example, if we dont pass string or boolean as parameter
+       in the neverSampleMethod , it will always throws an error. 
+      - Never type is used when the function/method  never returns any 
+       value while it has a return type. 
+      
+*/}
+
+ export class NeverExample {
+    neverSampleMethod(x : any): boolean {
+      if (typeof x === 'string') {
+        return true;
+      } else if (typeof x === 'bolean') {
+          return false;
+      }
+
+      return this.throwError('This method nevers ends');
+    }
+    throwError(text: string) : never {
+      throw new Error(text);
+    }
+ }
